b588816ed2d9606d48f59c5063aa38b98bed106d
ayusharyan <ayusharyan5801@gmail.com>
2025-07-23 19:07:36 +0530

    some changes

--- djangofirst/settings.py
+++ djangofirst/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
+    "theme",
 ]
+
+TAILWIND_APP_NAME = 'theme' 
+
+INTERNAL_IPS = ['127.0.0.1']
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -126,3 +135,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+ # (or whatever name you're going to use)
